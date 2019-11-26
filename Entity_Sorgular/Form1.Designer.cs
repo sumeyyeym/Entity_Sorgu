6fddc6707d9d4d5247480de400f361224da8fdf8
Sümeyye KAYA <sumeyye.kaya@baogrenci.com>
2019-11-26 12:57:06 +0300

    Add project files.

---
+++ Entity_Sorgular/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Entity_Sorgular
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOrn1 = new System.Windows.Forms.Button();
+            this.btnorn2 = new System.Windows.Forms.Button();
+            this.btnOrn3 = new System.Windows.Forms.Button();
+            this.btnOrn4 = new System.Windows.Forms.Button();
+            this.btnOrn5 = new System.Windows.Forms.Button();
+            this.btnOrn6 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnOrn1
+            // 
+            this.btnOrn1.Location = new System.Drawing.Point(12, 12);
+            this.btnOrn1.Name = "btnOrn1";
+            this.btnOrn1.Size = new System.Drawing.Size(75, 23);
+            this.btnOrn1.TabIndex = 0;
+            this.btnOrn1.Text = "Örnek 1";
+            this.btnOrn1.UseVisualStyleBackColor = true;
+            this.btnOrn1.Click += new System.EventHandler(this.BtnOrn1_Click);
+            // 
+            // btnorn2
+            // 
+            this.btnorn2.Location = new System.Drawing.Point(93, 12);
+            this.btnorn2.Name = "btnorn2";
+            this.btnorn2.Size = new System.Drawing.Size(75, 23);
+            this.btnorn2.TabIndex = 1;
+            this.btnorn2.Text = "Örnek 2";
+            this.btnorn2.UseVisualStyleBackColor = true;
+            this.btnorn2.Click += new System.EventHandler(this.Btnorn2_Click);
+            // 
+            // btnOrn3
+            // 
+            this.btnOrn3.Location = new System.Drawing.Point(174, 12);
+            this.btnOrn3.Name = "btnOrn3";
+            this.btnOrn3.Size = new System.Drawing.Size(75, 23);
+            this.btnOrn3.TabIndex = 2;
+            this.btnOrn3.Text = "Örnek 3";
+            this.btnOrn3.UseVisualStyleBackColor = true;
+            this.btnOrn3.Click += new System.EventHandler(this.BtnOrn3_Click);
+            // 
+            // btnOrn4
+            // 
+            this.btnOrn4.Location = new System.Drawing.Point(450, 12);
+            this.btnOrn4.Name = "btnOrn4";
+            this.btnOrn4.Size = new System.Drawing.Size(75, 23);
+            this.btnOrn4.TabIndex = 3;
+            this.btnOrn4.Text = "Örnek 4";
+            this.btnOrn4.UseVisualStyleBackColor = true;
+            this.btnOrn4.Click += new System.EventHandler(this.BtnOrn4_Click);
+            // 
+            // btnOrn5
+            // 
+            this.btnOrn5.Location = new System.Drawing.Point(531, 12);
+            this.btnOrn5.Name = "btnOrn5";
+            this.btnOrn5.Size = new System.Drawing.Size(75, 23);
+            this.btnOrn5.TabIndex = 4;
+            this.btnOrn5.Text = "Örnek 5";
+            this.btnOrn5.UseVisualStyleBackColor = true;
+            // 
+            // btnOrn6
+            // 
+            this.btnOrn6.Location = new System.Drawing.Point(612, 12);
+            this.btnOrn6.Name = "btnOrn6";
+            this.btnOrn6.Size = new System.Drawing.Size(75, 23);
+            this.btnOrn6.TabIndex = 5;
+            this.btnOrn6.Text = "Örnek 6";
+            this.btnOrn6.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 41);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(675, 253);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(699, 305);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnOrn6);
+            this.Controls.Add(this.btnOrn5);
+            this.Controls.Add(this.btnOrn4);
+            this.Controls.Add(this.btnOrn3);
+            this.Controls.Add(this.btnorn2);
+            this.Controls.Add(this.btnOrn1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOrn1;
+        private System.Windows.Forms.Button btnorn2;
+        private System.Windows.Forms.Button btnOrn3;
+        private System.Windows.Forms.Button btnOrn4;
+        private System.Windows.Forms.Button btnOrn5;
+        private System.Windows.Forms.Button btnOrn6;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Entity_Sorgular/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Entity_Sorgular
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        NorthwindEntities db = new NorthwindEntities();
+        private void BtnOrn1_Click(object sender, EventArgs e)
+        {
+            #region Link To Entity;
+            //dataGridView1.DataSource = db.Products.Where(x => x.UnitPrice >= 20 && x.UnitPrice <= 50)
+            //                                          .Select(x => new
+            //                                          {
+            //                                              UrunNo = x.ProductID,
+            //                                              UrunAdi = x.ProductName,
+            //                                              StokAdet = x.UnitsInStock,
+            //                                              Fiyat = x.UnitPrice
+            //                                          })
+            //                                          .OrderBy(x => x.Fiyat)
+            //                                          .ToList(); 
+            #endregion
+
+            #region Link to SQL
+            var result = from p in db.Products
+                         where p.UnitPrice >= 20 && p.UnitPrice <= 50
+                         orderby p.UnitPrice ascending
+                         select new
+                         {
+                             UrunNo = p.ProductID,
+                             UrunAdi = p.ProductName,
+                             StokAdet = p.UnitsInStock,
+                             Fiyat = p.UnitPrice
+                         };
+            dataGridView1.DataSource = result.ToList(); 
+            #endregion
+        }
+
+        private void Btnorn2_Click(object sender, EventArgs e)
+        {
+            var result = from x in db.Orders
+                         select new
+                         {
+                             x.Customer.CompanyName,
+                             Personel = x.Employee.FirstName + " " + x.Employee.LastName,
+                             x.OrderID,
+                             x.OrderDate,
+                             KargoAdi = x.Shipper.CompanyName
+                         };
+            dataGridView1.DataSource = result.ToList();
+
+            //dataGridView1.DataSource = db.Orders.Select(x => new
+            //{
+            //    x.Customer.CompanyName,
+            //    Personel = x.Employee.FirstName + " " + x.Employee.LastName, //eğer personel yazıp tanımlamazsak hata alırız. çünkü iki sütunu birleştirip yeni sütun oluşturduk ama veri tabanı üzerinde bu yeni sütunu tutacak bir nesne olmadığı için hata verir
+            //    x.OrderID,
+            //    x.OrderDate,
+            //    KargoAdi = x.Shipper.CompanyName //başında x.shipper yazmasına rağmen ram'e çıktığında bunu okumaz. bu sebeple yukarıda customer kısmındaki companyname'in de olmasından dolayı içeride iki tane companyname algılar ve hata verir. bunu başka bir nesneye atamak gerek.
+            //}).ToList(); //lazy load yaptık
+        }
+
+        private void BtnOrn3_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = db.Customers.Where(x => x.CompanyName.Contains("restaurant")).ToList();
+
+            //var result = from c in db.Customers
+            //             where c.CompanyName.Contains("restaurant")
+            //             select c;
+            //dataGridView1.DataSource = result.ToList();
+        }
+
+        private void BtnOrn4_Click(object sender, EventArgs e)
+        {
+            #region Yöntem 1
+            //Category cat = db.Categories.FirstOrDefault(x => x.CategoryName == "Beverages");
+            //Product prod = new Product();
+            //prod.ProductName = "Kola 1";
+            //prod.UnitPrice = 5.00m;
+            //prod.UnitsInStock = 500;
+            //prod.CategoryID = cat.CategoryID;
+
+            //db.Products.Add(prod);
+            //db.SaveChanges(); 
+            #endregion
+
+            db.Categories.FirstOrDefault(x => x.CategoryName == "Beverages").Products.Add(new Product
+            {
+                ProductName = "Kola 2",
+                UnitPrice = 5.00m,
+                UnitsInStock = 500
+            });
+            db.SaveChanges();
+
+            dataGridView1.DataSource = db.Products.Where(x => x.ProductName.StartsWith("Kola"))
+                .Select(x => new
+                {
+                    x.ProductName,
+                    x.UnitPrice,
+                    x.UnitsInStock,
+                    x.CategoryID
+                }).ToList();
+        }
+    }
+}
